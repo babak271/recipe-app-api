2c678b07759e1f4eec4299f7ab5868d7b69f0282
Babak <52350580+babak271@users.noreply.github.com>
2024-09-27 17:14:44 +0200

    fix flake

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -49,7 +49,9 @@ class TestPublicUser(TestCase):
         }
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exist = get_user_model().objects.filter(email=payload["email"]).exists()
+        user_exist = get_user_model().objects.filter(
+            email=payload["email"]
+        ).exists()
         self.assertFalse(user_exist)
 
     def test_user_token_success(self):
